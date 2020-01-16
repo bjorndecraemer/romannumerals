ef8d188d01c2e030a7b090a297981686542b54f8
Bjorn DC <bjorn.decraemer@yahoo.com>
2020-01-16 14:47:46 +0100

    IV plus I works

--- src/test/java/be/ordinakatas/romannumerals/RomanCalculatorTest.java
+++ src/test/java/be/ordinakatas/romannumerals/RomanCalculatorTest.java
@@ -11,42 +11,48 @@ class RomanCalculatorTest {
 
     @Test
     void addNumbers_IplusI_returnsII() {
-        String result = romanCalculator.addNumbers("I","I");
+        String result = romanCalculator.addNumbers("I", "I");
         assertThat(result).isEqualTo("II");
     }
 
     @Test
     void addNumbers_IplusII_returnsIII() {
-        String result = romanCalculator.addNumbers("I","II");
+        String result = romanCalculator.addNumbers("I", "II");
         assertThat(result).isEqualTo("III");
     }
 
 
     @Test
     void addNumbers_IIplusI_returnsIII() {
-        String result = romanCalculator.addNumbers("II","I");
+        String result = romanCalculator.addNumbers("II", "I");
         assertThat(result).isEqualTo("III");
     }
 
 
     @Test
     void addNumbers_IplusIII_returnsIV() {
-        String result = romanCalculator.addNumbers("I","III");
+        String result = romanCalculator.addNumbers("I", "III");
         assertThat(result).isEqualTo("IV");
     }
 
 
     @Test
     void addNumbers_IIplusII_returnsIV() {
-        String result = romanCalculator.addNumbers("II","II");
+        String result = romanCalculator.addNumbers("II", "II");
         assertThat(result).isEqualTo("IV");
     }
 
 
-
     @Test
     void addNumbers_IIplusIII_returnsV() {
-        String result = romanCalculator.addNumbers("II","III");
+        String result = romanCalculator.addNumbers("II", "III");
+        assertThat(result).isEqualTo("V");
+    }
+
+
+    @Test
+    void addNumbers_IVplusI_returnsV() {
+        String result = romanCalculator.addNumbers("IV", "I");
         assertThat(result).isEqualTo("V");
     }
 }
